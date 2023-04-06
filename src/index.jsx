baebcd09cf20c9a920dd93b94560b7645f1fbceb
strannik <spacenett55@gmail.com>
2023-04-06 04:08:11 +0400

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
 import { createGlobalStyle } from "styled-components";
-
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
